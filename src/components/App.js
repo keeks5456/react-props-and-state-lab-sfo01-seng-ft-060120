55edc51f70075055ec69734beb58228f80f7e0e3
keeks5456 <Akikogreen08@gmail.com>
2020-09-20 13:20:24 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,52 @@ class App extends React.Component {
 
     this.state = {
       pets: [],
+      isAdopted: false,
       filters: {
         type: 'all'
       }
     }
   }
+
+  onFindPetsClick = () =>{
+    const {type} = this.state.filters
+    const url = `/api/pets${type !== 'all' ? '?type=' + type : ''}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(pets => {this.setState({ pets: pets })})
+    // other option, this gives same results
+    // .then(pets => {this.setState({ pets })})
+  }
+
+  // dont forget that we passed in the pet.id from <Pet /> so we need to make sure that it gets passed as an argument in ur function we create for it.
+  onAdoptPet = (petId) =>{
+    console.log('hi', petId)
+
+    this.setState({
+      pets: this.state.pets.map(p => (p.id === petId ? {...p, isAdopted: true} : p))
+    })
+  // other option
+  // this.setState((prevState) => ({
+  //   pets: this.prevState.pets.map(p => (p.id === petId ? {...p, isAdopted: true} : p))
+  // }))
+
+  }//end
+
+    // change isAdopted state from false to true and back
+    // logic: find the pet.id and match it to the clicked petID,
+    // if these two are a match then change the state of isAdopted to true
+    // use the spread operator in setState function to change the state of that petID
+  // otherwise leave the state alone
+
+  onChangeType = (e) =>{
+    console.log('hi', e.target)
+        // let newType = e.target.value
+        this.setState({
+          filters:{
+            type: e.target.value
+          }
+        })
+      } //why isnt this working??
 
   render() {
     return (
@@ -24,10 +65,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+              onChangType={this.onChangeType}
+              onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,43 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  // can also do the isAdopted button logic in a function
+
+  // renderButton = () => {
+  //   if(this.props.pet.isAdopted){
+  //   return  <button className="ui disabled button">Already adopted</button>
+  //   } else {
+  //    return <button className="ui primary button">Adopt pet</button>
+  //   }
+  // }
+
+
   render() {
+
+    const {id, name, type, age, gender, weight, isAdopted} = this.props.pet
+  // is also valid 
+  // const {pet} = this.props
+
+  
     return (
       <div className="card">
         <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+          <a href="#" className="header">
+            {gender === 'male' ? '♂': '♀'}
+            {name}
+            {/*pet.name*/ }
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+       { /* this.renderButton */}
+        {isAdopted ? (<button className="ui disabled button">Already adopted</button> ): (<button className="ui primary button" onClick={()=> this.props.onAdoptPet(id)}>Adopt pet</button> ) }
         </div>
       </div>
     )
